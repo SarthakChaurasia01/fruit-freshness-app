95968156626ab2e3534a66b1c13734ab252fdef1
Sarthak Chaurasia <166941942+SarthakChaurasia01@users.noreply.github.com>
2025-11-02 22:28:30 +0530

    Remove training transforms and clean up code

--- transformers.py
+++ transformers.py
@@ -1,23 +1,10 @@
 from torchvision import transforms
 
-
 IMG_SIZE = 224
 
-
-train_transform = transforms.Compose([
-transforms.RandomResizedCrop(IMG_SIZE),
-transforms.RandomHorizontalFlip(p=0.5),
-transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
-transforms.ToTensor(),
-transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]),
-])
-
-
-
-
-# deterministic transform for inference
 test_transform = transforms.Compose([
-transforms.Resize((IMG_SIZE, IMG_SIZE)),
-transforms.ToTensor(),
-transforms.Normalize(mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]),
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
 ])
